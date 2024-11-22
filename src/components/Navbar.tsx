a10834d1600f1dfb635829568b1fd9ffec79700c
kohct1 <kohct2006@gmail.com>
2024-11-22 17:10:49 +0800

    feat: add checkout successful dialog

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,15 @@ import { doc, setDoc } from "firebase/firestore";
 
 import { Button } from "./ui/button";
 import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
     Sheet,
     SheetContent,
     SheetDescription,
@@ -62,7 +71,26 @@ function Navbar({ cart, setCart }: { cart: Product[], setCart: any }) {
                                 })}
                             </SheetHeader>
                             <div className="sticky bottom-0 bg-neutral-200 py-4">
-                                <Button className="w-full bg-white text-black hover:bg-neutral-300" onClick={handleCheckout}>Checkout</Button>
+                                <Dialog>
+                                    <DialogTrigger className="w-full bg-white rounded-md text-sm text-black font-medium py-2.5 hover:bg-neutral-300" onClick={handleCheckout}>Checkout</DialogTrigger>
+                                    <DialogContent className="bg-neutral-200">
+                                        <DialogHeader className="flex flex-col gap-2">
+                                            <div>
+                                                <DialogTitle>Successful Checkout</DialogTitle>
+                                                <DialogDescription>
+                                                    Thank you for your purchase!
+                                                </DialogDescription>
+                                            </div>
+                                            <div className="flex justify-center items-center">
+                                                <svg className="w-1/3 h-full" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path className="fill-green-500" d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                                            </div>
+                                            <h1 className="text-center">Head over to Telegram to complete the purchase</h1>
+                                            <DialogClose asChild>
+                                                <Button className="bg-white text-black hover:bg-neutral-300">Next</Button>
+                                            </DialogClose>
+                                        </DialogHeader>
+                                    </DialogContent>
+                                </Dialog>
                             </div>
                         </SheetContent>
                     </Sheet>
