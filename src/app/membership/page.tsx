ded7ec226b92b795f7b193b3162787d4de8fdbb5
Sam <samuelkoh17@gmail.com>
2025-07-04 21:03:16 +0800

    feat: add navbar to membership page

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/app/(site)/Navbar";
+
 export default function MembershipPage() {
   return (
     <div className="relative bg-black/20">
@@ -7,11 +9,16 @@ export default function MembershipPage() {
         alt="Background"
         className="fixed top-0 left-0 w-full h-full object-cover z-[-1]"
       />
+
+
+      <div>
+        <Navbar />
         <h1 className="text-4xl font-bold text-gray-900 mb-8">Membership</h1>
         <p className="text-lg text-gray-600">
           Welcome to our membership page! Here you can find information about our membership plans.
         </p>
         {/* Add your membership content here */}
       </div>
+    </div>
   );
 }
