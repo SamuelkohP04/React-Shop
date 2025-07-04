18a5400bf4d4744311042816f111053d4a5e94d9
Sam <samuelkoh17@gmail.com>
2025-07-04 23:43:43 +0800

    wip: using react bootstrap card module for each member benefit

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,4 +1,6 @@
 import Navbar from "@/app/(site)/Navbar";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function MembershipPage() {
   return (
@@ -17,6 +19,19 @@ export default function MembershipPage() {
         <p className="text-lg text-gray-600">
           Welcome to our membership page! Here you can find information about our membership plans.
         </p>
+
+      <Card style={{ width: '18rem' }}>
+      {/*<Card.Img variant="top" src="holder.js/100px180" />*/}
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  
         {/* Add your membership content here */}
       </div>
     </div>
