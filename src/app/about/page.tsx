878df77896bb6efba77dba09ffa991a5d74966e6
Sam <samuelkoh17@gmail.com>
2025-07-05 17:35:44 +0800

    feat: added background & about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Navbar from '../(site)/Navbar'
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Welcome to the About Us page!</p>
+    <div className="relative bg-black/20">
+      {/* Background GIF */}
+      <img
+        src="/LandingPage/LandingBackground.jpg"
+        alt="Background"
+        className="fixed top-0 left-0 w-full h-full object-cover z-[-1]"
+      />
+
+      <Navbar />
+    
+      <h1 className="text-4xl font-bold text-white m-8 pt-10 mx-auto">About Us</h1>
+      <p className="text-base sm:text-lg text-white m-4 sm:m-8 md:m-14 max-w-sm sm:max-w-md md:max-w-2xl mx-auto">
+        Welcome to the About Us page!
+      </p>
     </div>
   )
 }
