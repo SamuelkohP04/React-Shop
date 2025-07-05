b62f49f050ce5b0a559d0c4192825d1680264af5
Sam <samuelkoh17@gmail.com>
2025-07-05 17:56:57 +0800

    feat: boilerplate code of section with flexbox

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -41,9 +41,59 @@ export default function AboutPage() {
             face difficult situations to solve their personal problems & life
             issues. <br /> <br />I am also an experienced Energy Healer in
             Quantum Touch & Pranic Healing.
+
+
+            <br /> <br />
+            My mission is to help individuals achieve greater self-awareness, personal growth,
+            and spiritual enlightenment through the transformative power of Tarot
+            readings and energy healing.
+            
           </p>
         </div>
+
       </div>
+
+
+      <h1
+        className="text-4xl font-bold text-white pt-10
+      m-4 sm:m-8 md:m-14 max-w-sm sm:max-w-md md:max-w-2xl 
+      mx-auto"
+      >
+        Hello, I'm Benjamin Koh!
+      </h1>
+
+      <div className="flex flex-row items-center justify-center min-h-screen m-20">
+        <div className="w-14 flex-1">
+          <img
+            src="/LandingPage/FakePerson1.jpeg"
+            alt="Logo"
+            className="w-full h-auto"
+          />
+        </div>
+
+        <div className="w-28 flex-1">
+          <p className="text-base sm:text-lg text-white m-4 sm:m-8 md:m-14 max-w-sm sm:max-w-md md:max-w-2xl mx-auto">
+            I'm a Tarot Card Reading/Numerology Consultant with more than 12
+            years of experience. I provide assistance & guidance to clients who
+            face difficult situations to solve their personal problems & life
+            issues. <br /> <br />I am also an experienced Energy Healer in
+            Quantum Touch & Pranic Healing.
+
+
+            <br /> <br />
+            My mission is to help individuals achieve greater self-awareness, personal growth,
+            and spiritual enlightenment through the transformative power of Tarot
+            readings and energy healing.
+            
+          </p>
+        </div>
+
+    
+      </div>
+
+
+      
+
     </div>
   );
 }
