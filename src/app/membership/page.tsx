1131f7224e2f4d1a0078ab34f6c91e401e7c4af4
Sam <samuelkoh17@gmail.com>
2025-07-05 17:13:05 +0800

    refactor: organising code indentation and structure

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -7,19 +7,21 @@ export default function MembershipPage() {
     {
       title: "Basic Free Plan",
       price: "$0",
-      description:[
-        "24/7 Telegram & Website appointment booking access" ,
-        "Limited notifications from Telegram bot"]
+      description: [
+        "24/7 Telegram & Website appointment booking access",
+        "Limited notifications from Telegram bot",
+      ],
       //image: "/LandingPage/BasicPlan.jpg", // Replace with actual image path
     },
     {
       title: "Enlightenment Membership Plan",
       price: "$10",
-      description:
-        ["Birthday discounts: having a 10% discount on a purchase during birthday month" ,
-        "Discounted bundle of 10% off when booking a tarot card reading + energy crystal purchase" ,
-        "24/7 Telegram & Website appointment booking access" ,
-        "Birthday wishes & other unlimited notifications from Telegram bot"],
+      description: [
+        "Birthday discounts: having a 10% discount on a purchase during birthday month",
+        "Discounted bundle of 10% off when booking a tarot card reading + energy crystal purchase",
+        "24/7 Telegram & Website appointment booking access",
+        "Birthday wishes & other unlimited notifications from Telegram bot",
+      ],
       //image: "/LandingPage/PremiumPlan.jpg", // Replace with actual image path
     },
   ];
@@ -35,7 +37,10 @@ export default function MembershipPage() {
       <div>
         <Navbar />
         <h1 className="text-4xl font-bold text-white mb-8">Membership</h1>
-        <p className="text-lg text-white m-14 max-w-2xl mx-auto">
+        <p className="text-base sm:text-lg text-white m-4 sm:m-8 md:m-14 
+        max-w-sm sm:max-w-md md:max-w-2xl 
+        
+        mx-auto">
           Welcome to our membership page! Here you can find information about
           our membership plans.
         </p>
@@ -53,12 +58,13 @@ export default function MembershipPage() {
                 <p className="mt-2 text-black text-5xl font-bold">
                   {plan.price}
                 </p>
-                <ul className="mt-2 text-black text-sm space-y-4">{plan.description.map((point, index) => (
-                  <li key={index} className="block text-black list-disc ">
-                    {point}
-                  </li>
-                ))}</ul>
-
+                <ul className="mt-2 text-black text-sm space-y-4">
+                  {plan.description.map((point, index) => (
+                    <li key={index} className="block text-black list-disc ">
+                      {point}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
           ))}
